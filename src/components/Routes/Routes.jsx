b073c847dd11d673842ec8d4e45ea69dc428d224
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-08-01 21:46:30 +0300

    Added Sidebar Component

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,13 +1,27 @@
 /* eslint-disable react/prop-types */
 import { Route, Routes } from "react-router";
+import styled from "styled-components";
 import Header from "../Header";
+import Sidebar from "../Sidebar";
 
 const RoutesComp = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Header />} />
-    </Routes>
+    <>
+      <Header />
+      <AppBody>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          {/* Chat */}
+        </Routes>
+      </AppBody>
+    </>
   );
 };
 
 export default RoutesComp;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,82 @@
+import {
+  Add,
+  Apps,
+  BookmarkBorder,
+  Create,
+  ExpandLess,
+  ExpandMore,
+  FileCopy,
+  Inbox,
+  InsertComment,
+  PeopleAlt,
+  QuestionAnswer,
+  Send,
+} from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
+import SidebarOption from "./SidebarOption";
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <SidebarHeader>
+        <SidebarInfo>
+          <span>Slack</span>
+        </SidebarInfo>
+        <Create />
+      </SidebarHeader>
+      <SidebarOption Icon={InsertComment} title={"Threads"} />
+      <SidebarOption Icon={BookmarkBorder} title={"Later"} />
+      <SidebarOption Icon={QuestionAnswer} title={"Direct messages"} />
+      <SidebarOption Icon={Inbox} title={"Mentions & reactions"} />
+      <SidebarOption Icon={Send} title={"Drafts & sent"} />
+      <SidebarOption Icon={FileCopy} title={"Files"} />
+      <SidebarOption Icon={PeopleAlt} title={"People & user groups"} />
+      <SidebarOption Icon={Apps} title={"Apps"} />
+      <SidebarOption Icon={ExpandLess} title={"Show less"} />
+      <hr />
+      <SidebarOption Icon={ExpandMore} title={"Channels"} />
+      <hr />
+      <SidebarOption Icon={Add} addChannelOption title={"Add channels"} />
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
+
+const SidebarContainer = styled.div`
+  color: white;
+  background-color: var(--sideBar-slack-background-color);
+  flex: 0.2;
+  border-top: 1px solid #2a2b30;
+  max-width: 260px;
+  margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #2a2b30;
+  }
+`;
+const SidebarHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #2a2b30;
+  padding: 13px;
+  > .MuiSvgIcon-root {
+    padding: 8px;
+    color: #18171c;
+    font-size: 18px;
+    background-color: white;
+    border-radius: 999px;
+  }
+`;
+const SidebarInfo = styled.div`
+  flex: 1;
+  > span {
+    font-size: 18px;
+    font-weight: 900;
+    margin-bottom: 5px;
+  }
+`;
