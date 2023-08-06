5e9b463da753a774659b3503da0033ab26f7a6ba
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-08-06 23:00:28 +0300

    Added Loading With Spinner

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -7,9 +7,21 @@ import Sidebar from "../Sidebar";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Login from "../Login";
 import { auth } from "../../configs/firebase";
+import Spinner from "react-spinkit";
 const RoutesComp = () => {
   const [user, loading] = useAuthState(auth);
 
+  if (loading) {
+    <AppLoading>
+      <AppLoadingContents>
+        <img
+          src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+          alt=""
+        />
+        <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
+      </AppLoadingContents>
+    </AppLoading>;
+  }
   return (
     <>
       {!user ? (
@@ -31,6 +43,20 @@ const RoutesComp = () => {
 
 export default RoutesComp;
 
+const AppLoading = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
+const AppLoadingContents = styled.div``;
 const AppBody = styled.div`
   display: flex;
   height: 100vh;
