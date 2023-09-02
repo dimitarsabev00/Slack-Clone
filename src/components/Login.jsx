3dac7864727cba42089abe233c22f9d07e6ff13c
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-09-02 16:01:23 +0300

    Edit Content In Login Form In Login Screen Component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,8 +23,7 @@ const Login = () => {
           src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
           alt=""
         />
-        <h1>Sign in to Slack</h1>
-        <p>cloneSlack.slack.com</p>
+        <h1>Sign in to Slack Clone</h1>
         <Button onClick={signIn}>Sign in with Google</Button>
       </LoginInnerContainer>
     </LoginContainer>
