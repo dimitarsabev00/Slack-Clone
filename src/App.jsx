276f84dae1ca3bc36dafb8fff089d1056aa6446b
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-09-06 10:37:43 +0300

    Remove console.log(currentUser) & Get currentUser State From Redux In App.jsx File

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./components";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { checkUser } from "./store/slices/generalSlice";
 const App = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     if (localStorage.getItem("token") !== null) dispatch(checkUser());
   }, []);
-  const currentUser = useSelector(({ generalSlice }) => generalSlice.user);
-  console.log(currentUser);
+
   return (
     <Router>
       <Routes />
